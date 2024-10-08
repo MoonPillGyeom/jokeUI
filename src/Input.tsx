a6581fe86bf09169458acf3fa14e41ccaa7b2f0b
MoonPillGyeom <ansvkf0820@naver.com>
2024-10-08 15:40:15 +0900

    issue: 라이브러리 배포 이후 Test 컴포넌트 불러올 수 없는 이슈 해결중

---
+++ src/Input.tsx
@@ -0,0 +1,3 @@
+export default function Input({ placeholder }: InputProps) {
+  return <input placeholder={placeholder} style={{ width: '270px', height: '250px', borderBlockColor: 'red' }} />;
+}
--- src/components/input/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Input() {
-  return (
-    <>
-      <input className='w-8 h-5' placeholder='TestLibrary'></input>
-    </>
-  );
-}
