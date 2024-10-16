6feb1a9784a18a01a173e7bf6446817109adb076
MoonPillGyeom <ansvkf0820@naver.com>
2024-10-16 14:55:20 +0900

    style: button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,19 @@
-interface Props {
-  text: string;
+import './Button.css';
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
 }
 
-export default function Button({ text }: Props) {
-  return <div>Button</div>;
+export default function Button({ variant, ...props }: ButtonProps) {
+  const colorClasses = () => {
+    if (variant === 'primary') return { borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA' };
+    if (variant === 'secondary') return { borderColor: '#6366F1', backgroundColor: '#FAFAFA', color: '#6366F1' };
+    return {};
+  };
+
+  return (
+    <button style={{ ...colorClasses(), ...props.style }} {...props}>
+      {props.children}
+    </button>
+  );
 }
