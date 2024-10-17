9ce82beb471ff012c74b814e7799d5864c7ef6a2
MoonPillGyeom <ansvkf0820@naver.com>
2024-10-17 12:15:53 +0900

    feat: add storybook, testCode

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Button.css';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -6,13 +7,14 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 
 export default function Button({ variant, ...props }: ButtonProps) {
   const colorClasses = () => {
+    if (props.disabled) return {};
     if (variant === 'primary') return { borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA' };
     if (variant === 'secondary') return { borderColor: '#6366F1', backgroundColor: '#FAFAFA', color: '#6366F1' };
     return {};
   };
 
   return (
-    <button style={{ ...colorClasses(), ...props.style }} {...props}>
+    <button className='pill-button test' style={{ ...colorClasses(), ...props.style }} {...props}>
       {props.children}
     </button>
   );
